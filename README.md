660d7fb6e0c2c7df642177e43374b2de4a31d75f
ghofranjdaradkh <ghofran_daradkh@hotmail.com>
2023-10-18 14:29:06 +0300

    taskmaster

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # taskmaster
+
+
+Main page :
+![Alt text](screenshots/Capture.PNG)
+
+
+Addtask page :
+allow users to type in details about a new task
+![Alt text](screenshots/addTask.PNG)
+
+
+alltasks page :
+![Alt text](screenshots/alltasks.PNG)
+
+
---
+++ app/src/main/java/com/example/taskmaster/ALLTASKS.java
@@ -0,0 +1,28 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ALLTASKS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alltasks);
+
+
+        TextView addTask = findViewById(R.id.textBar2);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(ALLTASKS.this, MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+    }
+}
